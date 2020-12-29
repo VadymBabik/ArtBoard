027b0bcfa2fc07df0a35b886226577283dbb0373
Vadym Babik <vadym.babik@gmail.com>
2020-12-30 00:33:15 +0200

    swiper_start

--- src/js/script.js
+++ src/js/script.js
@@ -8,25 +8,3 @@ function open() {
   document.querySelector(".fa-times").classList.toggle("show");
   document.querySelector(".fa-bars").classList.toggle("show");
 }
-
-$(document).ready(function () {
-  $(".instagram__gallery").slick({
-    slidesToShow: 4,
-
-    // variableWidth: true,
-    responsive: [
-      {
-        breakpoint: 320,
-        settings: {
-          slidesToShow: 1,
-        },
-      },
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 3,
-        },
-      },
-    ],
-  });
-});
